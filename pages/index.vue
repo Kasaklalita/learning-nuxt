8ab117155010d16e911ff4136c932f731a2c723e
Veniamin Polienko <kasaklalita@mail.ru>
2023-02-24 13:56:31 +0300

    Nuxt links

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,15 @@
-vbase
-<v-base></v-base>
 <template>
   <div>
+    <header>
+      <nav>
+        <NuxtLink to="/">Nuxt Dojo</NuxtLink>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/products">Products</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
     <h2>Home</h2>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod fuga esse
