549da5b913d681f86155d9034db86b1684e32162
Veniamin Polienko <kasaklalita@mail.ru>
2023-02-24 14:31:45 +0300

    Fetching data

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,11 +1,19 @@
 <template>
-  <div>Products</div>
+  <div>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="p in products">
+        <NuxtLink :to="`/products/${p.id}`">{{ p.title }}</NuxtLink>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
 definePageMeta({
   layout: "products",
 });
+// Fetch the products
+const { data: products } = await useFetch("https://fakestoreapi.com/products");
 </script>
 
 <style scoped></style>
