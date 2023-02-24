e49b2f25378e2e1554fda838b95c488fbb627a8a
Veniamin Polienko <kasaklalita@mail.ru>
2023-02-24 13:11:05 +0300

    Initial commit

---
+++ components/AppAlert.vue
@@ -0,0 +1,10 @@
+<template>
+  <span style="color: red">
+    <slot />
+  </span>
+</template>
+<script setup lang="ts">
+const route = useRoute();
+// When accessing /posts/1, route.params.id will be 1
+console.log(route.params.id);
+</script>
