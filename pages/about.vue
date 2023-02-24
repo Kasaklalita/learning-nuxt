754f8dc14ad5db0b3b897a4d4c0158185266c7d0
Veniamin Polienko <kasaklalita@mail.ru>
2023-02-24 13:43:00 +0300

    Adding pages

---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+vbase
+<v-base></v-base>
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod fuga esse
+      sit aperiam corporis modi fugit ducimus recusandae tempore quaerat!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod fuga esse
+      sit aperiam corporis modi fugit ducimus recusandae tempore quaerat!
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
