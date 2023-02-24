76cbce9cb269e920a19855bdc8bb616a4974b7ab
Veniamin Polienko <kasaklalita@mail.ru>
2023-02-24 15:06:38 +0300

    Reusable components

---
+++ components/ProductDetails.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card">
+    <div class="grid grid-cols-2 gap-10">
+      <div class="p-7">
+        <img
+          :src="props.product.image"
+          alt="product image"
+          class="mx-auto my-7"
+        />
+      </div>
+      <div class="p-7">
+        <h2 class="text-4xl my-7">{{ props.product.title }}</h2>
+        <p class="text-xl my-7">Price - ${{ props.product.price }}</p>
+        <h3 class="font-bold border-b-2 mb-4 pb-2">Product description:</h3>
+        <p class="mb-7">{{ props.product.description }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(["product"]);
+</script>
+
+<style scoped>
+img {
+  max-width: 400px;
+}
+</style>
